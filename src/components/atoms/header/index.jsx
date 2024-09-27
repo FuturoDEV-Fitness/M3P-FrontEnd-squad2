35e879be5091e1cda67f7f08fb37914aa29ca0c4
Guilherme Betsa <ybtyza@gmail.com>
2024-09-27 13:30:27 -0300

    feat: 🎸 (Outlet) Condição para verificar se usuario esta logado

--- src/components/atoms/header/index.jsx
+++ src/components/atoms/header/index.jsx
@@ -1,10 +1,12 @@
 import logo from "../../../assets/FitHubLogo.png";
+import { Link } from "react-router-dom";
 
 import CButton from "../CButton";
-
 import styles from "./index.module.css";
 
 function Header() {
+  const tokenJWT = JSON.parse(localStorage.getItem("tokenJWT"));
+
   return (
     <div className={styles.navContainer}>
       <nav className={styles.nav}>
@@ -12,30 +14,49 @@ function Header() {
           <img src={logo} alt="logo" />
           <h1 onClick={() => (window.location.href = "/")}>FitHub!</h1>
         </div>
-
-        {/* Precisa ser feito uma condicional para saber se o usuário esta logado */}
-        <div className={styles.links}>
-          <CButton
-            variant="outlined"
-            sx={{
-              color: "#EFF6E0",
-              borderColor: "#EFF6E0",
-              "&:hover": { backgroundColor: "#eff6e0", color: "#124559" },
-            }}
-          >
-            Entrar
-          </CButton>
-          <CButton
-            variant="outlined"
-            sx={{
-              color: "#EFF6E0",
-              borderColor: "#EFF6E0",
-              "&:hover": { backgroundColor: "#eff6e0", color: "#124559" },
-            }}
-          >
-            Cadastre-se
-          </CButton>
-        </div>
+        {tokenJWT ? (
+          <div className={styles.links}>
+            <Link to="/lista-exercicios">Meus Locais</Link>
+            <Link to="/cadastro-exercicios">Cadastrar Locais</Link>
+            <Link to="/perfil">Perfil</Link>
+            <CButton
+              onClick={() => {
+                logout(JSON.parse(localStorage.getItem("tokenJWT")));
+              }}
+              variant="outlined"
+              sx={{
+                color: "#EFF6E0",
+                borderColor: "#EFF6E0",
+                "&:hover": { backgroundColor: "#eff6e0", color: "#124559" },
+              }}
+            >
+              Sair
+            </CButton>
+          </div>
+        ) : (
+          <div className={styles.links}>
+            <CButton
+              variant="outlined"
+              sx={{
+                color: "#EFF6E0",
+                borderColor: "#EFF6E0",
+                "&:hover": { backgroundColor: "#eff6e0", color: "#124559" },
+              }}
+            >
+              Entrar
+            </CButton>
+            <CButton
+              variant="outlined"
+              sx={{
+                color: "#EFF6E0",
+                borderColor: "#EFF6E0",
+                "&:hover": { backgroundColor: "#eff6e0", color: "#124559" },
+              }}
+            >
+              Cadastre-se
+            </CButton>
+          </div>
+        )}
       </nav>
     </div>
   );
