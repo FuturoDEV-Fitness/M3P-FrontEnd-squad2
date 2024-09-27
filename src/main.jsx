b4d2b2ec2126659352f78093fd3e854722993c7c
Bruno Viscardi <bruno.viscardi33@gmail.com>
2024-09-27 16:21:21 -0300

    feat: 🎸 (UsuariosContext) Criação do contexto

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,16 @@
-import { createRoot } from 'react-dom/client'
-import { RouterProvider } from 'react-router-dom'
-import { router } from './routers/router'
-import { CepContextProvider } from './context/CepContext'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { RouterProvider } from "react-router-dom";
+import { router } from "./routers/router";
+import { CepContextProvider } from "./context/CepContext";
+import { UsuariosContextProvider } from "./context/UsuariosContext";
 
-import './index.css'
+import "./index.css";
 
-createRoot(document.getElementById('root')).render(
-  <CepContextProvider>
-    <RouterProvider router={router} />
-  </CepContextProvider>
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+ <CepContextProvider>
+  <UsuariosContextProvider>
+   <RouterProvider router={router} />
+  </UsuariosContextProvider>
+ </CepContextProvider>
+);
